0eb139d508b825024f12991d57166cab2d25f29e
Maagot <maagotrs@gmail.com>
2021-07-24 03:11:32 +0000

    If nesting is shit

--- ThinkJava/Chapter5/Triangle.java
+++ ThinkJava/Chapter5/Triangle.java
@@ -12,50 +12,51 @@ public class Triangle {
         System.out.print("Type A variable: ");
         
         if (!in.hasNextDouble()) {
-            final String word = in.next();
-            System.err.println("\"" + word + "\" is not allowed, only numbers!");
+            double positiveCheck = in.nextDouble();
 
-        } else {
-            final double a = in.nextDouble();
-            System.out.println("Variable a is: " + a);
-
-            //Get and check for Double variable b
-
-            System.out.print("Type B variable: ");
-
-            if (!in.hasNextDouble()) {
+            if (positiveCheck <= 0) {
                 final String word = in.next();
-                System.err.println("\"" + word + "\" is not allowed, only numbers!");
+                System.err.println("\"" + word + "\" is not allowed, only positive numbers!");
 
+        
             } else {
-                final double b = in.nextDouble();
-                System.out.println("Variable b is: " + b);
+                final double a = in.nextDouble();
+                System.out.println("Variable a is: " + a);
 
-                // Get and check for Double variable C
+                //Get and check for Double variable b
 
-                System.out.print("Type c variable: ");
+                System.out.print("Type B variable: ");
 
-                if (!in.hasNextDouble()) {
-                    final String word = in.next();
-                    System.err.println("\"" + word + "\" is not allowed, only numbers!");
-
-                } else {
-
-                    final double c = in.nextDouble();
-                    System.out.println("Variable c is: " + c);
-
-                    if (a + b > c || a + c > b || c + b > a) {
-                        System.out.println("Sorry, but you can't create a triangle :c");
+                    if (!in.hasNextDouble()) {
+                        final String word = in.next();
+                        System.err.println("\"" + word + "\" is not allowed, only numbers!");
 
                     } else {
-                        System.out.println("Yea, you can make a triangle c:");
+                        final double b = in.nextDouble();
+                        System.out.println("Variable b is: " + b);
 
+                        // Get and check for Double variable C  
+
+                        System.out.print("Type c variable: ");
+
+                        if (!in.hasNextDouble()) {
+                            final String word = in.next();
+                            System.err.println("\"" + word + "\" is not allowed, only numbers!");
+
+                        } else {
+                            final double c = in.nextDouble();
+                            System.out.println("Variable c is: " + c);
+
+                            if (a + b < c || a + c < b || c + b < a) {
+                                System.out.println("Sorry, but you can't create a triangle :c");
+
+                            } else {
+                                System.out.println("Yea, you can make a triangle c:");
+
+                        }
                     }
-
                 }
             }
         }
-
-
     }
 }
