060a962e0677c2328c02421267ece7e1daa9ff0b
Maagot <maagotrs@gmail.com>
2021-09-10 04:24:17 +0300

    intellij Started exercise 7.4

--- ThinkJava/Chapter7/Sieve.java
+++ ThinkJava/Chapter7/Sieve.java
@@ -1,4 +1,24 @@
 package ThinkJava.Chapter7;
 
+import java.util.Arrays;
+
 public class Sieve {
+    public static void main(String[] args) {
+        sieveMethod(6);
+
+    }
+
+    public static void sieveMethod(int index) {
+        int[] sieveArray = new int[index];
+
+        for (int i = 0; i <= sieveArray.length - 1; i++) {
+            sieveArray[i] = i;
+
+        }
+
+
+
+        System.out.println(Arrays.toString(sieveArray));
+
+    }
 }
