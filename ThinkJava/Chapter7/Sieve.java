debe069986aae47f120120b5434c79af03284a95
Maagot <maagotrs@gmail.com>
2021-09-18 06:18:55 +0300

    intellij Started exercise 7.4

--- ThinkJava/Chapter7/Sieve.java
+++ ThinkJava/Chapter7/Sieve.java
@@ -4,21 +4,43 @@ import java.util.Arrays;
 
 public class Sieve {
     public static void main(String[] args) {
-        sieveMethod(6);
+//        sieveMethod(120);
 
     }
 
-    public static void sieveMethod(int index) {
-        int[] sieveArray = new int[index];
+    public static boolean areFactors(int n, int[] fCheck) {
 
-        for (int i = 0; i <= sieveArray.length - 1; i++) {
-            sieveArray[i] = i;
+        return false;
+
+    }
+
+    public static boolean[] sieveMethod(int index) {
+        // allocate memory for array
+        int[] sieveArray = new int[index];
+        boolean[] sieveBooleanArray = new boolean[index];
+
+        // fill array with numbers from 1 to n
+        for (int i = 0; i < sieveArray.length; i++) {
+            sieveArray[i] = i + 1;
+
+            if (sieveArray[i] == 2 || sieveArray[i] == 3 || sieveArray[i] == 5) {
+                sieveBooleanArray[i] = true;
+
+            } else if (sieveArray[i] % 2 != 0 && sieveArray[i] % 3 != 0 && sieveArray[i] % 5 != 0 && sieveArray[i] % 7 != 0) {
+                sieveBooleanArray[i] = true;
+
+            }
 
         }
 
-
-
         System.out.println(Arrays.toString(sieveArray));
+        System.out.println(Arrays.toString(sieveBooleanArray));
+        System.out.println(sieveArray[98 - 2]);
+        System.out.println(sieveBooleanArray[98 - 2]);
+
+
+
+        return sieveBooleanArray;
 
     }
 }
